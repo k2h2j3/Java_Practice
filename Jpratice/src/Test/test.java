e1616136a03dcfd265776133865b9b34a6e6635f
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-07-27 16:42:55 +0900

    Add files via upload

--- Jpratice/src/Test/test.java
+++ Jpratice/src/Test/test.java
@@ -13,21 +13,36 @@ public class test {
 
         for(int tc = 1; tc <= T; tc++) {
 
-            int N = sc.nextInt();
-            System.out.println("#" + tc);
+            int P = sc.nextInt();
+            int Q = sc.nextInt();
+            int R = sc.nextInt();
+            int S = sc.nextInt();
+            int W = sc.nextInt();
 
-            for(int i = 1; i <= N; i++){
+            //A회사 B회사에 지불할 수도금액
+            int costA = 0;
+            int costB = 0;
+            //가장 합리적인 수도금액
+            int bestcost = 0;
 
-                String inputStr = sc.next();
-                int inputNum = sc.nextInt();
-                System.out.println();
-
-                for(int j = 0; j < inputNum; j++){
-
-                    System.out.print(inputStr);
-                }
-
+            //B사 수도 금액 계산
+            if(W > R){
+                costB = Q + (W-R)*S;
             }
+            else{
+                costB = Q;
+            }
+
+            //가장 합리적인 가격 도출
+            if(costB >= W * P){
+                costA = W * P;
+                bestcost =  costA;
+            }
+            else if(costB < W * P){
+                bestcost = costB;
+            }
+            System.out.println("#"+ tc + " " + bestcost);
+
         }
 
 
