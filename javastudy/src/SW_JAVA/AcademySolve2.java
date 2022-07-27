a49d0d4001272c7b69b4f5d8b85c70ac124458ca
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-07-27 10:03:33 +0900

    Add files via upload

--- javastudy/src/SW_JAVA/AcademySolve2.java
+++ javastudy/src/SW_JAVA/AcademySolve2.java
@@ -142,8 +142,30 @@ public class AcademySolve2 {
 
         }*/
 
+        //SW Academy 1989 : 회문 찾기
 
+           /* Scanner sc = new Scanner(System.in);
+            int T;
+            T = sc.nextInt();
 
+            for(int i = 1; i <= T; i++) {
+                String N;
+                N = sc.next();
+                char[] arr = N.toCharArray();
+                int Max = arr.length;
+                int result = 0;
+                for(int j = 0; j < Max; j++){
+                    if(arr[j] == arr[Max-j-1]){
+                        result = 1;
+                    }
+                    else{
+                        result = 0;
+                    }
+                }
+                System.out.print("#" + i + " " + result);
+                System.out.println();
+
+            }*/
 
 
     }
