2c0bc6d1167a54ad4459e76cdc7a67ae3f7e214d
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-07-30 11:46:55 +0900

    Add files via upload

--- Jpratice/src/Test/test.java
+++ Jpratice/src/Test/test.java
@@ -9,65 +9,55 @@ public class test
 
         Scanner sc = new Scanner(System.in);
 
-
-        for (int tc = 1; tc <= 10; tc++) {
+        for(int tc = 1; tc <= 10; tc++){
+            int ladderNum = sc.nextInt();
             int[][] ladder = new int[100][100];
-            int N = sc.nextInt();
 
-
-            //사다리 배열 만들기
-            for (int i = 0; i < ladder.length; i++) {
-                for (int j = 0; j < ladder[i].length; j++) {
+            //사다리 배열 생성
+            for(int i = 0; i < ladder.length; i++){
+                for(int j = 0; j < ladder[i].length; j++){
                     int input = sc.nextInt();
                     ladder[i][j] = input;
                 }
             }
-            //사다리 이동거리 값을 저장하는 변수
-            int[] data = countLength(ladder);
 
-            int min = data[0];
-            int result = 0;
-            for (int i = 0; i < data.length; i++){
-                if(data[i] != 0 && min >= data[i]){
-                    min = data[i];
-                    result = i;
-                }
-            }
+            int result = findX(ladder);
             System.out.println("#" + tc + " " + result);
+
         }
 
     }
 
-    public static int[] countLength(int[][] ladder) {
-        int[] length = new int[100];
-
+    public static int findX(int[][] ladder){
+        int Xindex = 0;
         for(int i = 0; i < 100; i++) {
-            int row = 0;   int col = i;
-            int cnt = 0;
+            int row = 0;
+            int col = i;
             int[][] passed = new int[100][100];
             passed[row][col] = 1;
-            if (ladder[row][col] == 0) {
-                continue;
-            }
-            while (row < 100) {
-                if (col - 1 >= 0 && ladder[row][col - 1] == 1 && passed[row][col-1] == 0) {
-                    col--;
-                    passed[row][col] = 1;
-                } else if (col + 1 < 100 && ladder[row][col + 1] == 1 && passed[row][col+1] == 0) {
-                    col++;
-                    passed[row][col] = 1;
-                } else {
-                    row++;
-                    if (row < 100) {
+            if (ladder[0][col] == 1) {
+                while (row < 100) {
+                    if (ladder[99][col] == 2) {
+                        Xindex = i;
+                        break;
+                    }
+                    if (col - 1 >= 0 && ladder[row][col - 1] == 1 && passed[row][col] == 0) {
+                        col--;
                         passed[row][col] = 1;
+                    } else if (col + 1 < 100 && ladder[row][col + 1] == 1 && passed[row][col] == 0) {
+                        col++;
+                        passed[row][col] = 1;
+                    } else {
+                        row++;
+                        if (row < 100) {
+                            passed[row][col] = 1;
+                        }
                     }
                 }
-                cnt++;
-            }
-            length[i] = cnt;
 
+            }
         }
-        return length;
+        return Xindex;
     }
 
 }
--- javastudy/src/SW_JAVA/AcademySolveD4/ladder1.java
+++ javastudy/src/SW_JAVA/AcademySolveD4/ladder1.java
@@ -3,69 +3,59 @@ import java.util.*;
 
 //SW Academy 1210 : 사다리 타기 1
 public class ladder1 {
-    public static int[][] ladder = new int[100][100];
-    public static boolean find = false;
     public static void main(String[] args) {
-
-
-
 
         Scanner sc = new Scanner(System.in);
 
+        for(int tc = 1; tc <= 10; tc++){
+            int ladderNum = sc.nextInt();
+            int[][] ladder = new int[100][100];
 
-        for (int tc = 1; tc <= 10; tc++) {
-
-            int result = 0;
-            int N = sc.nextInt();
-            find = false;
-
-
-            //사다리 배열 만들기
-            for (int i = 0; i < ladder.length; i++){
+            //사다리 배열 생성
+            for(int i = 0; i < ladder.length; i++){
                 for(int j = 0; j < ladder[i].length; j++){
                     int input = sc.nextInt();
                     ladder[i][j] = input;
                 }
             }
-            for (int i = 0; i < 100; i++){
-                //스타팅 지점 찾기
-                if (ladder[0][i] == 1) {
-                    //사다리 타기
-                    findX(0, i);
-                    //2 발견하면 i지점 반환
-                    if (find) {
-                        result = i;
+
+            int result = findX(ladder);
+            System.out.println("#" + tc + " " + result);
+
+        }
+
+    }
+
+    public static int findX(int[][] ladder){
+        int Xindex = 0;
+        for(int i = 0; i < 100; i++) {
+            int row = 0;
+            int col = i;
+            int[][] passed = new int[100][100];
+            passed[row][col] = 1;
+            if (ladder[0][col] == 1) {
+                while (row < 100) {
+                    if (ladder[99][col] == 2) {
+                        Xindex = i;
                         break;
                     }
+                    if (col - 1 >= 0 && ladder[row][col - 1] == 1 && passed[row][col] == 0) {
+                        col--;
+                        passed[row][col] = 1;
+                    } else if (col + 1 < 100 && ladder[row][col + 1] == 1 && passed[row][col] == 0) {
+                        col++;
+                        passed[row][col] = 1;
+                    } else {
+                        row++;
+                        if (row < 100) {
+                            passed[row][col] = 1;
+                        }
+                    }
                 }
-            }
-            System.out.println("#"+ tc + " " + result);
 
-
-
-        }
-
-
-}
-    //사다리 타기 1 메서드
-    public static void findX(int col, int row) {
-        boolean route[][] = new boolean[100][100];
-        while(col < 100){
-            route[col][row] = true;
-            if(ladder[col][row] == 2){
-                find = true;
-                break;
-            }
-            if(row + 1 < 100 && ladder[col][row+1] == 1 && route[col][row+1] == false){
-                row++;
-            }
-            else if(row - 1 >= 0 && ladder[col][row-1] == 1 && route[col][row-1] == false){
-                row--;
-            }
-            else{
-                col++;
             }
         }
+        return Xindex;
     }
 
 
