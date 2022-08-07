94ae3ddef6803d42e531993eeb26ac5c62e9be9c
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-08-07 20:03:06 +0900

    Add files via upload

--- javastudy/src/baekjoon/baekjoon1978.java
+++ javastudy/src/baekjoon/baekjoon1978.java
@@ -1,7 +1,9 @@
+package baekjoon;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baekjoon1978 {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
@@ -43,8 +45,3 @@ public class Main {
 
 
 }
-
-
-
-
-
