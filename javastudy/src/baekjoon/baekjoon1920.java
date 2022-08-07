615477e291c28e25f75a3db908721bbea70c3564
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-08-08 00:15:09 +0900

    Add files via upload

--- javastudy/src/baekjoon/baekjoon1920.java
+++ javastudy/src/baekjoon/baekjoon1920.java
@@ -1,7 +1,9 @@
+package baekjoon;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baekjoon1920 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -60,8 +62,3 @@ public class Main {
 
 
 }
-
-
-
-
-
