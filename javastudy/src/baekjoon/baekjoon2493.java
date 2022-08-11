f3e9b77860357ceae2572228ec3d293eee586128
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-08-11 15:32:16 +0900

    Add files via upload

--- javastudy/src/baekjoon/baekjoon2493.java
+++ javastudy/src/baekjoon/baekjoon2493.java
@@ -1,9 +1,11 @@
+package baekjoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class baekjoon2493 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,15 +35,3 @@ public class Main {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
