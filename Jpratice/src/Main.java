2a8d4b98237c01e710e776daac6f17238ea6e504
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-08-16 00:11:18 +0900

    Add files via upload

--- Jpratice/src/Main.java
+++ Jpratice/src/Main.java
@@ -1,56 +1,86 @@
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
+    static int N;
+    static int[] mr = {-1,0,1,0};
+    static int[] mc = {0,-1,0,1};
+    static boolean[][] check;
+    static int[][] map;
+
     public static class Point{
-        Point(int m,int t){
-            me = m;
-            time = t;
-        }
-        int me,time;
+        Point(int r, int c){
+            row = r;
+            col = c;
+
+        }int row,col;
     }
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
 
-        int me = sc.nextInt();
-        int sister = sc.nextInt();
+        N = sc.nextInt();
 
-        int result = bfs(me,sister);
-        System.out.println(result);
+        map = new int[N][N];
 
-    }
-    public static int bfs(int me,int sister){
-
-        boolean[] check = new boolean[200000];
-        Queue<Point> q = new ArrayDeque<>();
-
-        check[me] = true;
-        q.add(new Point(me,0));
-
-        while(!q.isEmpty()){
-
-            Point now = q.remove();
-
-            if(now.me == sister){
-                return now.time;
-            }
-
-            if(now.me + 1 <= 199999 && !check[now.me+1]){
-                check[now.me+1] = true;
-                q.add(new Point(now.me+1, now.time+1));
-            }
-            if(now.me * 2 <= 199999 && !check[now.me*2]){
-                check[now.me*2] = true;
-                q.add(new Point(now.me*2, now.time+1));
-            }
-            if(now.me - 1 >= 0 && !check[now.me-1]){
-                check[now.me-1] = true;
-                q.add(new Point(now.me-1, now.time+1));
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                map[i][j] = sc.nextInt();
             }
         }
-        return -1;
+
+        int[] result = new int[101];
+        int cnt = 0;
+
+        for(int r = 0; r < 101; r++){
+            cnt = 0;
+            check = new boolean[N][N];
+
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(map[i][j] > r && !check[i][j]){
+                        cnt++;
+                        bfs(i,j,r);
+                    }
+                }
+            }
+            result[r] = cnt;
+        }
+        Arrays.sort(result);
+        System.out.print(result[100]);
+    }
+
+    public static void bfs(int r,int c, int rain){
+
+        Queue<Point> q = new ArrayDeque<>();
+
+        check[r][c] = true;
+        q.add(new Point(r,c));
+
+        while(!q.isEmpty()){
+            Point now = q.remove();
+            for(int i = 0; i < 4; i++){
+                int nr = now.row + mr[i];
+                int nc = now.col + mc[i];
+
+                if(nr < 0 || nr > N-1 || nc < 0 || nc > N-1){
+                    continue;
+                }
+                if(map[nr][nc] <= rain){
+                    continue;
+                }
+                if(check[nr][nc]){
+                    continue;
+                }
+
+                check[nr][nc] = true;
+                q.add(new Point(nr, nc));
+            }
+        }
     }
 }
 
