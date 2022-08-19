37843b69533b6519d1da29bdb2dc6c4ecfd2d068
Kim Seung Gyeom <74819625+k2h2j3@users.noreply.github.com>
2022-08-19 22:56:31 +0900

    Add files via upload

--- Jpratice/src/Main.java
+++ Jpratice/src/Main.java
@@ -1,34 +1,67 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
+    static int N;
+    static int[][] paper;
+    static int blue,white;
+
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
 
-        int result = Integer.MAX_VALUE;
+        blue = 0;
+        white = 0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        paper = new int[N][N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-
-        while( st.hasMoreTokens()){
-            int temp = 0;
-
-            StringTokenizer st_2 = new StringTokenizer(st.nextToken(),"+");
-
-            while(st_2.hasMoreTokens()){
-                temp += Integer.parseInt(st_2.nextToken());
-            }
-            if(result == Integer.MAX_VALUE){
-                result = temp;
-            }
-            else{
-                result -= temp;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                paper[i][j] = sc.nextInt();
             }
         }
-        System.out.println(result);
+
+        recursion(0,0,N);
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+    public static void recursion(int r, int c, int n){
+
+        if(check(r,c,n)){
+            if(paper[r][c] == 0){
+                white++;
+            }
+            else{
+                blue++;
+            }
+            return;
+        }
+        int half = n/2;
+        //1사분면
+        recursion(r,c+half,n/2);
+        //2사분면
+        recursion(r,c,n/2);
+        //3사분면
+        recursion(r+half,c, n/2);
+        //4사분면
+        recursion(r+half,c+half,n/2);
+    }
+    public static boolean check(int r, int c, int n){
+
+        int sample = paper[r][c];
+
+        for(int i = r; i < r + n; i++){
+            for(int j = c; j < c +n; j++){
+                if(sample != paper[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
 
