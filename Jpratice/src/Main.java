5445ad237b83499ea734ecf3d750e40fa0a996b2
Kim Seung Gyeom <74819625+k2h2j3@users.noreply.github.com>
2022-08-20 09:02:22 +0900

    Add files via upload

--- Jpratice/src/Main.java
+++ Jpratice/src/Main.java
@@ -3,65 +3,69 @@ import java.util.*;
 
 public class Main {
 
-    static int N;
-    static int[][] paper;
-    static int blue,white;
-
     static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb2 = new StringBuilder();
+    static int N,M;
+    static int[][] map;
+    static boolean[] check;
 
     public static void main(String[] args) throws IOException {
 
-        blue = 0;
-        white = 0;
-
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
-        paper = new int[N][N];
+        M = sc.nextInt();
+        int V =sc.nextInt();
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                paper[i][j] = sc.nextInt();
-            }
+        map = new int[N+1][N+1];
+
+        for(int i = 0; i < M; i++){
+            int S_node = sc.nextInt();
+            int E_node = sc.nextInt();
+
+            map[S_node][E_node] = map[E_node][S_node] = 1;
         }
+        check = new boolean[N+1];
+        dfs(V);
+        bfs(V);
 
-        recursion(0,0,N);
-
-        System.out.println(white);
-        System.out.println(blue);
+        System.out.println(sb2);
+        System.out.println(sb);
     }
-    public static void recursion(int r, int c, int n){
 
-        if(check(r,c,n)){
-            if(paper[r][c] == 0){
-                white++;
-            }
-            else{
-                blue++;
-            }
-            return;
-        }
-        int half = n/2;
-        //1사분면
-        recursion(r,c+half,n/2);
-        //2사분면
-        recursion(r,c,n/2);
-        //3사분면
-        recursion(r+half,c, n/2);
-        //4사분면
-        recursion(r+half,c+half,n/2);
-    }
-    public static boolean check(int r, int c, int n){
+    public static void bfs(int v){
 
-        int sample = paper[r][c];
+        check = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
 
-        for(int i = r; i < r + n; i++){
-            for(int j = c; j < c +n; j++){
-                if(sample != paper[i][j]){
-                    return false;
+        sb.append(v).append(" ");
+        check[v] = true;
+        q.add(v);
+
+        while(!q.isEmpty()){
+            int temp = q.remove();
+
+            for(int i = 0 ;i < N+1; i++){
+                if(map[temp][i] == 1 && !check[i]){
+                    check[i] = true;
+                    q.add(i);
+                    sb.append(i).append(" ");
                 }
             }
+
         }
-        return true;
+    }
+
+    public static void dfs(int v){
+
+        sb2.append(v).append(" ");
+        check[v] = true;
+
+        for(int i = 1; i < N+1; i++) {
+            if (map[v][i] == 1 && !check[i]) {
+                dfs(i);
+            }
+        }
     }
 }
 
